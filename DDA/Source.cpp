080d1af9612c098d36129196c0e51e541b747647
dimdum354 <dimz.smallforward@gmail.com>
2017-10-31 13:57:03 +0700

    Add project files.

---
+++ DDA/Source.cpp
@@ -0,0 +1,108 @@
+// Algoritma DDA
+
+#include <GL\freeglut.h>
+#include <GL\glut.h>
+#include <iostream>
+
+using namespace std;
+
+//identifier fungsi
+void init();
+void display(void);
+void dda(void);
+
+//  posisi window di layar
+int window_x;
+int window_y;
+
+//  ukuran window
+int window_width = 480;
+int window_height = 480;
+
+//  judul window
+char *judul_window = "Algoritma DDA";
+
+void main(int argc, char **argv)
+{
+	//  inisialisasi GLUT (OpenGL Utility Toolkit)
+	glutInit(&argc, argv);
+	// set posisi window supaya berada di tengah 
+	window_x = (glutGet(GLUT_SCREEN_WIDTH) - window_width) / 2;
+	window_y = (glutGet(GLUT_SCREEN_HEIGHT) - window_height) / 2;
+	glutInitWindowSize(window_width, window_height); //set ukuran window 
+	glutInitWindowPosition(window_x, window_y); //set posisi window
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE); // set display RGB dan double buffer
+	glutCreateWindow(judul_window);
+
+
+	init();
+
+	glutDisplayFunc(display); // fungsi display
+	glutMainLoop(); // loop pemrosesan GLUT
+}
+
+void init()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0); //set warna background 
+	glColor3f(1.0, 1.0, 1.0); //set warna titik
+	glPointSize(2.0); //set ukuran titik
+	glMatrixMode(GL_PROJECTION); //set mode matriks yang digunakan 
+	glLoadIdentity(); // load matriks identitas
+	gluOrtho2D(0.0, 600.0, 0.0, 600.0); // set ukuran viewing window
+}
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT); //clear color
+	dda(); //panggil fungsi dda 
+	glutSwapBuffers(); //swap buffer 
+}
+
+void dda(void) {
+
+	//tentukan titik awal dan akhir
+	int x1 = 3;
+	int x2 = 500;
+	int y1 = 5;
+	int y2 = 500;
+	double step;
+	double dx;
+	double dy, i;
+
+	double xInc, x, y;
+	double yInc;
+	x = x1;
+	y = y1;
+
+
+	//hitung dx dan dy
+	dx = x2 - x1;
+	dy = y2 - y1;
+	//hitung steps
+	if (dx>dy) {
+		step = dx;
+	}
+	else {
+		step = dy;
+	}
+
+	//hitung perubahan nilai x (x_inc) dan y (y_inc)
+	xInc = dx / step;
+	yInc = dy / step;
+
+	//gambar titik awal
+	glBegin(GL_POINTS);
+	glVertex2i(x, y); // gambar titik awal
+
+					  //perulangan untuk menggambar titik-titik 
+	for (i = x1; i < step; i++)
+	{
+
+		x += xInc;
+		y += yInc;
+		glVertex2i(round(x), round(y));
+
+	}
+	glEnd();
+	glFlush();
+}
